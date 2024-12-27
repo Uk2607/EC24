4f955e4610fd67f9fe403e5a3f2bf7516de53b9b
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-27 21:19:52 +0530

    d9 p3 partial

--- 09/q09.cpp
+++ 09/q09.cpp
@@ -57,11 +57,29 @@ void part_2(vector<int>arr) {
 
 void part_3(vector<int>arr) {
     vector<int>validStamps = {1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101};
-    int res = 0;
-    for(int x: arr)
-        res+=knapsack(validStamps, x);
-    cout<<"PART 3 :: "<<res<<"\n";
+    int res = 0, idx = 0;
+    for(int T: arr) {
+        cout<<idx++<<": "<<T<<" ";
+        int lmn = INT_MAX, rmn = INT_MAX;
+        for(int y=(T/2)-50;y<=T/2;y++) {
+            int x = T-y;
+            int l = knapsack(validStamps, y);
+            int r = knapsack(validStamps, x);
+            lmn = min(lmn, l);
+            rmn = min(rmn, r);
+        }
+        cout<<lmn<<", "<<rmn<<"\n";
+        res += (lmn+rmn);
+        // res += knapsack(validStamps, T);
+    }
+    cout<<"PART 3 :: "<<res<<"\n"; // 1 _ _ _ _ _ != 148844
 }
+// x>y
+// x+y=T;
+// x-y<=100
+
+// (1) --> x<=50+(T/2)
+// (2) --> y>=(T/2)-50
 
 int main() {
     int part;

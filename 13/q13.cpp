4aae42405166c7da15334318a5c8e479fdc3cf8d
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-06 21:33:49 +0530

    d13 p1 dij

--- 13/q13.cpp
+++ 13/q13.cpp
@@ -28,8 +28,49 @@ vector<string> read_data(string filePath) {
     return arr;
 }
 
+vector<pair<int,int>>dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+bool isInside(int x, int y, int H, int W) { return x>=0 && x<H && y>=0 && y<W; }
+
+int cyclicDiff(int a, int b) {
+    return min(abs(a-b), 10-abs(a-b));
+}
+
+// Dijkstra's algorithm
+int min_path(vector<string>&grid, int H, int W, pair<int,int>me, pair<int,int>to) {
+    vector<vector<bool>>vis(H, vector<bool>(W, false));
+    vector<vector<int>>dist(H, vector<int>(W, INT_MAX));
+    set<pair<int,pair<int, int>>>st;
+    st.insert({0, me});
+    dist[me.first][me.second] = 0;
+    while(!st.empty()) {
+        me = st.begin()->second;
+        st.erase(st.begin());
+        for(pair<int,int>d:dir) {
+            int nx = me.first+d.first, ny = me.second+d.second;
+            if(isInside(nx, ny, H, W) && grid[nx][ny]!='#' && !vis[nx][ny]) {
+                int new_dist = dist[me.first][me.second] + cyclicDiff((grid[nx][ny]-'0'), (grid[me.first][me.second]-'0')) + 1;
+                if(new_dist < dist[nx][ny]) {
+                    if(dist[nx][ny] != INT_MAX) st.erase({dist[nx][ny], {nx, ny}});
+                    dist[nx][ny] = new_dist;
+                    st.insert({dist[nx][ny], {nx, ny}});
+                }
+            }
+        }
+    }
+    return dist[to.first][to.second];
+}
+
 void part_1(vector<string>arr) {
-    cout<<"PART 1 :: "<<arr.size()<<"\n";
+    int H = arr.size(), W = arr[0].length();
+    pair<int,int>me, to;
+    for(int i=0;i<H;i++)
+        for(int j=0;j<W;j++)
+            if(arr[i][j] == 'S') { me = {i, j}; arr[i][j] = '0'; }
+            else if(arr[i][j] == 'E') { to = {i, j}; arr[i][j] = '0'; }
+    
+    int res = min_path(arr, H, W, me, to);
+    cout<<"PART 1 :: "<<res<<"\n"; // 143
 }
 
 void part_2(vector<string>arr) {

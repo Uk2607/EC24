adf126e38fdf78ada9c33cf39a529d6fb0ee6dc4
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-12 04:38:35 +0530

    d15 p2 ip

--- 15/q15.cpp
+++ 15/q15.cpp
@@ -28,8 +28,42 @@ vector<string> read_data(string filePath) {
     return arr;
 }
 
+vector<pair<int,int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+bool isInside(pair<int,int>&p, int H, int W) {
+    return p.first>=0 && p.first<H && p.second>=0 && p.second<W;
+}
+
+int get_min_path(vector<string>&arr, pair<int,int>&me, int H, int W) {
+    vector<vector<bool>>vis(H, vector<bool>(W, false));
+    queue<pair<int,int>>q;
+    int len = 0;
+    q.push(me);
+    while(!q.empty()) {
+        int sz = q.size();
+        while(sz--) {
+            auto p = q.front();
+            q.pop();
+            if(arr[p.first][p.second] == 'H') return 2*len;
+            for(pair<int,int> d: dirs) {
+                pair<int,int>new_p = {p.first+d.first, p.second+d.second};
+                if(isInside(new_p, H, W) && arr[new_p.first][new_p.second] != '#' && !vis[new_p.first][new_p.second]) {
+                    vis[new_p.first][new_p.second] = true;
+                    q.push(new_p);
+                }
+            }
+        }
+        len++;
+    }
+    return len*2;
+}
+
 void part_1(vector<string>arr) {
-    cout<<"PART 1 :: "<<arr.size()<<"\n";
+    pair<int,int>me;
+    int H = arr.size(), W = arr[0].length();
+    for(int j=0;j<W;j++) if(arr[0][j] == '.') { me = {0, j}; break; }
+    int min_path = get_min_path(arr, me, H, W);
+    cout<<"PART 1 :: "<<min_path<<"\n"; // 196
 }
 
 void part_2(vector<string>arr) {

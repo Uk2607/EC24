141c67def285509a741c19e12b8767c35ee5bbc7
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-14 00:18:57 +0530

    d17 p1

--- 17/q17.cpp
+++ 17/q17.cpp
@@ -29,8 +29,56 @@ vector<string> read_data(string filePath) {
     return arr;
 }
 
+int findParent(int node, vector<int>parent) {
+    if(parent[node] == -1) return node;
+    return findParent(parent[node], parent);
+}
+
+void unionSets(vector<int>& parent, int u, int v) {
+    int pu = findParent(u, parent), pv = findParent(v, parent);
+    parent[pu] = pv;
+}
+
+vector<pair<int,pair<int,int>>> kruskal(vector<pair<int,pair<int,int>>> edges, vector<pair<int,int>> pos) {
+    // Kruskal Algorithm
+    int n = pos.size();
+    sort(edges.begin(), edges.end());
+    vector<int> parent(n, -1);
+    vector<pair<int,pair<int,int>>> mst;
+
+    for(auto edge: edges) {
+        int u = edge.second.first, v = edge.second.second;
+        int pu = findParent(u, parent), pv = findParent(v, parent);
+        if(pu != pv) {
+            mst.push_back(edge);
+            unionSets(parent, pu, pv);
+        }
+    }
+
+    return mst;
+}
+
 void part_1(vector<string>arr) {
-    cout<<"PART 1 :: "<<arr.size()<<"\n";
+    vector<pair<int,int>> pos;
+    int H = arr.size(), W = arr[0].size();
+    for(int i=0;i<H;i++) for(int j=0;j<W;j++) if(arr[i][j] == '*') pos.push_back({i,j});
+    vector<pair<int,pair<int,int>>> edges;
+    for(int i=0;i<pos.size();i++)
+        for(int j=i+1;j<pos.size();j++) {
+            int dist = abs(pos[i].first-pos[j].first) + abs(pos[i].second-pos[j].second);
+            edges.push_back({dist, {i,j}});
+        }
+    // MST: Kruskal for sparse graph and Prims for dense graph
+    vector<pair<int,pair<int,int>>> mst = kruskal(edges, pos);
+
+    int res = pos.size();
+    for(int i=0;i<mst.size();i++) {
+        int u = mst[i].second.first, v = mst[i].second.second;
+        cout<<u+1<<" "<<v+1<<" "<<mst[i].first<<"\n";
+        res+=mst[i].first;
+    }
+
+    cout<<"PART 1 :: "<<res<<"\n";
 }
 
 void part_2(vector<string>arr) {

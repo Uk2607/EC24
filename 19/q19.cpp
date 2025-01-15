9691aad35c5ea3a118e1208210a59e368ab4a84a
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-15 07:22:51 +0530

    d19 ip

--- 19/q19.cpp
+++ 19/q19.cpp
@@ -11,33 +11,36 @@ using namespace std;
 #define ll long long
 #define ull unsigned long long
 
-vector<string> read_data(string filePath) {
+pair<string, vector<string>> read_data(string filePath) {
 
     ifstream file(filePath);
     vector<string> arr;
 
-    string line;
+    string ins, line;
 
     if (!file.is_open()) {
         cerr << "Failed to open file: " << filePath << endl;
-        return arr;
+        return {ins, arr};
     }
+    bool flag = false;
     while(getline(file, line)) {
-        arr.push_back(line);
+        if(line.length() == 0) {flag = true; continue;}
+        if(flag) arr.push_back(line);
+        else ins = line;
     }
     file.close();
-    return arr;
+    return {ins, arr};
 }
 
-void part_1(vector<string>arr) {
+void part_1(string ins, vector<string>arr) {
     cout<<"PART 1 :: "<<arr.size()<<"\n";
 }
 
-void part_2(vector<string>arr) {
+void part_2(string ins, vector<string>arr) {
     cout<<"PART 2 :: "<<arr.size()<<"\n";
 }
 
-void part_3(vector<string>arr) {
+void part_3(string ins, vector<string>arr) {
     cout<<"PART 3 :: "<<arr.size()<<"\n";
 }
 
@@ -45,30 +48,30 @@ int main() {
     int part;
     cout<<"Enter question part: ";
     cin>>part;
-    vector<string> ip;
+    pair<string, vector<string>> ip;
     
     string folder_path = "19/";
     switch (part)
     {
     case 1:
         ip = read_data(folder_path+"01.in");
-        part_1(ip);
+        part_1(ip.first, ip.second);
         break;
     case 2:
         ip = read_data(folder_path+"02.in");
-        part_2(ip);
+        part_2(ip.first, ip.second);
         break;
     case 3:
         ip = read_data(folder_path+"03.in");
-        part_3(ip);
+        part_3(ip.first, ip.second);
         break;
     default:
         ip = read_data(folder_path+"01.in");
-        part_1(ip);
+        part_1(ip.first, ip.second);
         ip = read_data(folder_path+"02.in");
-        part_2(ip);
+        part_2(ip.first, ip.second);
         ip = read_data(folder_path+"03.in");
-        part_3(ip);
+        part_3(ip.first, ip.second);
         break;
     }
     return 0;

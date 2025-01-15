7677fb86594365aee8eeff08f0b82c6674baa514
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-15 08:56:39 +0530

    d20 p1 ip

---
+++ 20/q20.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<cassert>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+vector<string> read_data(string filePath) {
+
+    ifstream file(filePath);
+    vector<string> arr;
+
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << filePath << endl;
+        return arr;
+    }
+    while(getline(file, line)) {
+        arr.push_back(line);
+    }
+    file.close();
+    return arr;
+}
+
+void part_1(vector<string>arr) {
+    cout<<"PART 1 :: "<<arr.size()<<"\n";
+}
+
+void part_2(vector<string>arr) {
+    cout<<"PART 2 :: "<<arr.size()<<"\n";
+}
+
+void part_3(vector<string>arr) {
+    cout<<"PART 3 :: "<<arr.size()<<"\n";
+}
+
+int main() {
+    int part;
+    cout<<"Enter question part: ";
+    cin>>part;
+    vector<string> ip;
+    
+    string folder_path = "20/";
+    switch (part)
+    {
+    case 1:
+        ip = read_data(folder_path+"01.in");
+        part_1(ip);
+        break;
+    case 2:
+        ip = read_data(folder_path+"02.in");
+        part_2(ip);
+        break;
+    case 3:
+        ip = read_data(folder_path+"03.in");
+        part_3(ip);
+        break;
+    default:
+        ip = read_data(folder_path+"01.in");
+        part_1(ip);
+        ip = read_data(folder_path+"02.in");
+        part_2(ip);
+        ip = read_data(folder_path+"03.in");
+        part_3(ip);
+        break;
+    }
+    return 0;
+}

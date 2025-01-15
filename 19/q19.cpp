86e8e05a6f848d1687fa30668d8a8bd44ad6a22f
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-15 07:46:02 +0530

    d19 p2

--- 19/q19.cpp
+++ 19/q19.cpp
@@ -78,7 +78,29 @@ void part_1(string ins, vector<string>arr) {
 }
 
 void part_2(string ins, vector<string>arr) {
-    cout<<"PART 2 :: "<<arr.size()<<"\n";
+    int H = arr.size(), W = arr[0].size(), n = ins.length(), idx;
+    int t = 100;
+    while(t--) {
+        idx = 0;
+        for(int i=1;i<H-1;i++) {
+            for(int j=1;j<W-1;j++) {
+                rotate(arr, {i, j}, ins[idx], H, W);
+                idx = (idx+1)%n;
+            }
+        }
+    }
+    string res = "";
+    bool flag = false;
+    for(int i=0;i<H;i++) {
+        for(int j=0;j<W;j++) {
+            // cout<<arr[i][j];
+            if(arr[i][j] == '>') flag = true;
+            else if(arr[i][j] == '<') flag = false;
+            else if(flag) res += arr[i][j];
+        }
+        // cout<<"\n";
+    }
+    cout<<"PART 2 :: "<<res<<"\n";
 }
 
 void part_3(string ins, vector<string>arr) {

c6bcf78c9c1d4b9136ab5d9012c095e13a32d213
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-16 19:03:08 +0530

    d12

--- 12/q12.cpp
+++ 12/q12.cpp
@@ -5,6 +5,7 @@
 #include<string>
 #include<map>
 #include<set>
+#include<tuple>
 using namespace std;
 
 #define ll long long
@@ -25,6 +26,27 @@ vector<string> read_data(string filePath) {
         arr.push_back(line);
     }
     arr.pop_back();
+    file.close();
+    return arr;
+}
+
+vector<pair<int,int>> read_data3(string filePath) {
+
+    ifstream file(filePath);
+    vector<pair<int,int>> arr;
+
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << filePath << endl;
+        return arr;
+    }
+    while(getline(file, line)) {
+        stringstream ss(line);
+        int x, y;
+        ss>>x>>y;
+        arr.push_back({x, y});
+    }
     file.close();
     return arr;
 }
@@ -53,8 +75,84 @@ void part_1n2(vector<string>arr) {
     cout<<"PART 1 :: "<<res<<"\n";
 }
 
-void part_3(vector<string>arr) {
-    cout<<"PART 3 :: "<<arr.size()<<"\n";
+tuple<int, int, int, int, int> calc_hit(pair<int,int>t, pair<int,int>p, int m) {
+    auto [rt, ct] = t;
+    auto [rs, cs] = p;
+    int t_delay = 0;
+    if ((ct - cs) & 1) {
+        t_delay = 1;
+        rt -= 1;
+        ct -= 1;
+    }
+
+    int delta = ct - cs;
+    int t_hit = delta / 2;
+    int r_hit = rt - delta / 2;
+    int c_hit = ct - delta / 2;
+    int dr = r_hit - rs;
+    int dc = c_hit - cs;
+
+    if (r_hit < 0) {
+        return make_tuple(-1, -1, -1, -1, -1); // None equivalent
+    }
+    if (dc < dr) {
+        return make_tuple(-1, -1, -1, -1, -1); // None equivalent
+    }
+
+    // hit in phase 1
+    int p_hit = dr;
+    if (dr == dc) {
+        return make_tuple(t_delay + t_hit, r_hit, c_hit, p_hit, m);
+    }
+
+    // hit in phase 2
+    p_hit = dr;
+    if (p_hit < dc && dc <= 2 * p_hit) {
+        return make_tuple(t_delay + t_hit, r_hit, c_hit, p_hit, m);
+    }
+
+    // hit in phase 3
+    p_hit = (dr + t_hit) / 3;
+    int r = (dr + t_hit) % 3;
+    if (r == 0) {
+        return make_tuple(t_delay + t_hit, r_hit, c_hit, p_hit, m);
+    }
+
+    return make_tuple(-1, -1, -1, -1, -1); // None equivalent
+}
+
+void part_3(vector<pair<int,int>>targets) {
+    int res = 0;
+    vector<pair<pair<int,int>,int>>players = {{{0, 0}, 1}, {{0, 1}, 2}, {{0, 2}, 3}};
+    for(pair<int,int>t: targets) {
+        vector<tuple<int, int, int, int, int>> hits;
+        for(auto [p, m]: players) {
+            auto hit = calc_hit(t, p, m);
+            if (get<0>(hit) != -1) { // Check if hit is not None
+                hits.push_back(hit);
+            }
+        }
+        if (!hits.empty()) {
+            int highest = numeric_limits<int>::min();
+            for (const auto& h : hits) {
+                highest = max(highest, get<1>(h));
+            }
+
+            vector<tuple<int, int, int, int, int>> hits_highest;
+            for (const auto& h : hits) {
+                if (get<1>(h) == highest) {
+                    hits_highest.push_back(h);
+                }
+            }
+
+            int min_product = numeric_limits<int>::max();
+            for (const auto& h : hits_highest) {
+                min_product = min(min_product, get<3>(h) * get<4>(h));
+            }
+            res += min_product;
+        }
+    }
+    cout<<"PART 3 :: "<<res<<"\n";
 }
 
 int main() {
@@ -62,6 +160,7 @@ int main() {
     cout<<"Enter question part: ";
     cin>>part;
     vector<string> ip;
+    vector<pair<int,int>> ip3;
     
     string folder_path = "12/";
     switch (part)
@@ -75,16 +174,16 @@ int main() {
         part_1n2(ip);
         break;
     case 3:
-        ip = read_data(folder_path+"03.in");
-        part_3(ip);
+        ip3 = read_data3(folder_path+"03.in");
+        part_3(ip3);
         break;
     default:
         ip = read_data(folder_path+"01.in");
         part_1n2(ip);
         ip = read_data(folder_path+"02.in");
         part_1n2(ip);
-        ip = read_data(folder_path+"03.in");
-        part_3(ip);
+        ip3 = read_data3(folder_path+"03.in");
+        part_3(ip3);
         break;
     }
     return 0;
